e7956add94284d90793abc613ce2af6dc975cc43
devluizf <luizfurtadomkt@hotmail.com>
2022-05-01 17:07:37 -0300

    feat: implementa o router de /graph

---
+++ src/routes/GraphRouter.ts
@@ -0,0 +1,18 @@
+import { GraphController } from '../controllers';
+import IGraph from '../interfaces/GraphInterface';
+import createGraphSchema from '../joi_schemas/createGraphSchema';
+import JoiValidator from '../middlewares/validator';
+import BaseRouter from './BaseRouter';
+
+class GraphRouter extends BaseRouter<IGraph> {
+  constructor(
+    controller = new GraphController(),
+    createValidator = new JoiValidator(createGraphSchema),
+    updateValidator = undefined,
+  ) {
+    super(controller, createValidator, updateValidator);
+  }
+
+}
+
+export default GraphRouter;
